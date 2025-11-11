45119d88f4addcb634e3367f1b75f85a9efec293
MiguelMalla09 <89275647+JetstreamSam09@users.noreply.github.com>
2025-11-10 22:57:05 -0500

    Create 3.js

---
+++ algoritmosciclosp1/3.js
@@ -0,0 +1,8 @@
+/*3. Algoritmo para generar la tabla de un numero dado por argumento en una función*/
+
+for(let i=1; i<=10; 1++){
+    num = 7
+    i=7
+    res = i * num
+    console.log(i +"x" +num +"=" +num)
+}
