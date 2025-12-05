82ae6cf8fb27cfcb95295da316d2bb74b3a93a52
MiguelMalla09 <89275647+JetstreamSam09@users.noreply.github.com>
2025-12-05 15:31:39 -0500

    Update 6.js

--- algoritmosciclosp1/6.js
+++ algoritmosciclosp1/6.js
@@ -1,16 +1,16 @@
 /*6. Escribe una función que reciba un array de números y devuelva un nuevo array que contenga solo los números pares.*/
 
-let numeros = [1,2,3,4,5,6,7,8,9,12,13,14,15,16,17,18,19];
+let numeros = [1,2,3,4,5,6,7,8,9,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
-contador = 0;
-function darmePares(array){
-    let pares = [];
-    for(let i=0; i<=numeros.length; 1++){
-        if(numeros[i]%2===0){
-            pares.push(numeros[i]);
-        }
+function darmePares(array) {
+  let pares = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      pares.push(array[i]);
     }
-    return pares
+  }
+  return pares;
 }
+
 let arrayNuevo = darmePares(numeros);
-console.log(arrayNuevo)
+console.log(arrayNuevo);
