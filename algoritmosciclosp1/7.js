f624a749098567a2b3124c5bb851fe0399bc3c17
MiguelMalla09 <89275647+JetstreamSam09@users.noreply.github.com>
2025-12-05 15:33:13 -0500

    Update 7.js

--- algoritmosciclosp1/7.js
+++ algoritmosciclosp1/7.js
@@ -1,13 +1,11 @@
 /*7. Implementa una función que calcule la suma de los cuadrados de los primeros N números naturales.*/
 
-let numeros = [4,5,2,3,8,10,3,4,7]
-function numeroMayor (array){
-    let numeroMayor = 0;
-    for(let i=0; i<=numeroMayor.length; i++ ){
-        if(array[i]>array[i+1]){
-            numeroMayor=array[i];
-        }
-    }
-    return numeroMayor;
+function sumaCuadrados(N) {
+  let suma = 0;
+  for (let i = 1; i <= N; i++) {
+    suma += i * i;
+  }
+  return suma;
 }
-console.log(numeroMayor(numeros));
+
+console.log(sumaCuadrados(54));
